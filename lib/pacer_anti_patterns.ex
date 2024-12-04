f45786d641a12089f90dcd743b8aef2592c8b0e4
De Wet Blomerus <dewetblomerus@gmail.com>
2024-12-04 14:05:09 -0500

    mix new pacer_anti_patterns

---
+++ lib/pacer_anti_patterns.ex
@@ -0,0 +1,18 @@
+defmodule PacerAntiPatterns do
+  @moduledoc """
+  Documentation for `PacerAntiPatterns`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PacerAntiPatterns.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pacer_anti_patterns_test.exs
@@ -0,0 +1,8 @@
+defmodule PacerAntiPatternsTest do
+  use ExUnit.Case
+  doctest PacerAntiPatterns
+
+  test "greets the world" do
+    assert PacerAntiPatterns.hello() == :world
+  end
+end
